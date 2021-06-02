097cc0ef545fb749d19141d9d8864d6b8423f7c6
Zoldig <hraphaelh@gmail.com>
2021-06-02 21:51:08 +0200

    [Update] add slider

--- LodeAuto/src/app/app.component.ts
+++ LodeAuto/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+export interface IDocuments {
+  src: string;
+  name: string;
+  description: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'LodeAuto';
+  public slides: IDocuments[] = [
+    {
+      src: '../assets/images/carousel-1.jpg',
+      name: 'img1',
+      description: 'voiture',
+    },
+    {
+      src: '../assets/images/carousel-2.jpg',
+      name: 'img2',
+      description: 'voiture',
+    },
+  ];
 }
--- LodeAuto/src/app/slider/slider.component.ts
+++ LodeAuto/src/app/slider/slider.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IDocuments } from '../app.component';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
+  @Input() slides!: IDocuments[];
+  currentIndex:number=0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  next(){
+    if(this.currentIndex<this.slides.length-1){
+      this.currentIndex++
+    }else{
+      this.currentIndex=0;
+    }
   }
-
+  prev(){
+    if(this.currentIndex>0){
+      this.currentIndex--;
+    }else{
+      this.currentIndex=this.slides.length-1;
+    }
+  }
 }
